fa2c6be93604e0d44c1eac16db3f7a65c35a1f8a
h-sara <115357536+h-sara@users.noreply.github.com>
2023-01-14 09:12:24 +0000

    [Fix] membersテーブルのカラムの制約を修正

--- db/schema.rb
+++ db/schema.rb
@@ -70,11 +70,12 @@ ActiveRecord::Schema.define(version: 2023_01_12_071551) do
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
     t.string "nickname", null: false
-    t.text "introduction", null: false
+    t.text "introduction"
     t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["nickname"], name: "index_members_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
