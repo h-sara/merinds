cc4907badebd269d0c146d6ac572928ed091fcd1
h-sara <115357536+h-sara@users.noreply.github.com>
2023-01-14 08:05:26 +0000

    [Add] ゲストログイン機能の追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,15 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |member|
+      member.password = SecureRandom.urlsafe_base64
+      memberuser.first_name = "guest"
+      memberuser.last_name = "member"
+      memberuser.first_name_kana = "ゲスト"
+      memberuser.last_name_kana = "メンバー"
+      memberuser.nickname = "merindsゲスト"
+    end
+  end
 end
