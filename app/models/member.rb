d3e2fc6686b40d2829d9a4e48825b170bfff2320
h-sara <115357536+h-sara@users.noreply.github.com>
2023-01-15 07:01:01 +0000

    [Add] メンバーマイページの作成

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,20 @@
 class Member < ApplicationRecord
+  has_one_attached :member_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+  # メンバーのアイコン画像の設定
+  def get_member_image(width, height)
+    unless member_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      member_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    # member_image.variant(resize_to_fill: [width, height]).processed
+  end
 
   # ゲストログインに使用するguestメソッドを定義
   def self.guest
