5b192be448f8b6bcb4b5c191aa4f0141866abc06
h-sara <115357536+h-sara@users.noreply.github.com>
2023-01-16 09:51:07 +0000

    [Add] 投稿機能の追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,8 +1,15 @@
 class Public::PostsController < ApplicationController
+
   def index
   end
 
   def show
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to post_path(@post.id)
   end
 
   def edit
@@ -19,4 +26,15 @@ class Public::PostsController < ApplicationController
 
   def show_your
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :sentence,
+      :image_id,
+      :is_edited,
+      :is_hidden).merge(member_id: current_member.id)
+  end
+
 end
--- db/migrate/20230112064658_create_posts.rb
+++ db/migrate/20230112064658_create_posts.rb
@@ -6,8 +6,8 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.integer :member_id, null: false
       ## 投稿内容／text型／NOTNULL
       t.text :sentence, null: false
-      ## 添付画像ID／string型／NOTNULL
-      t.integer :image_id, null: false
+      ## 添付画像ID／string型
+      t.integer :image_id
       ## 編集済みフラグ／boolean型／NOTNULL／default:false
       t.boolean :is_edited, null: false, default: false
       ## 非表示フラグ／boolean型／NOTNULL／default:false
