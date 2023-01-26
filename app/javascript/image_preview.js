bf9d0e6801907571a0b52cfc20e8319998e6f981
h-sara <115357536+h-sara@users.noreply.github.com>
2023-01-26 08:24:49 +0000

    [Add] 画像のプレビューを追加

---
+++ app/javascript/image_preview.js
@@ -0,0 +1,16 @@
+/*global $*/
+$(function () {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('.img_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $('.img_field').change(function () {
+    readURL(this);
+  });
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,8 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 
+import "image_preview.js"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
